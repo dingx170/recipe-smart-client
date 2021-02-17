e94ffebdca6fd87aebacf67c7f8703e42a39880b
tongding <dingx170@gmail.com>
2021-02-17 11:06:40 -0800

    updated new recipe

--- src/app/components/myrecipe/newrecipe/newrecipe.component.ts
+++ src/app/components/myrecipe/newrecipe/newrecipe.component.ts
@@ -12,15 +12,52 @@ export class NewrecipeComponent implements OnInit {
     {step: "prepare one cup oil"},
     {step: "crack 2 eggs"},
   ];
-
+  
   public ingredient_list:any[] = [
     {name: "flour", unit: "gram", count:300},
     {name: "oil", unit: "cup", count:1},
     {name: "egg", unit: "gram", count:150},
   ];
+
+  public allergies: string[] = [
+    "Corn", "Egg", "Fish", "Meat", "Milk", "Peanut", "Shellfish", "Soy", "TreeNut", "Wheat", "FPIES"
+  ]
+  public cuisines: string[] = [
+    "-- Options --", "Chinese", "Mexican", "Italian", "Japanese", "Greek", "French", "Thai", "Spanish", "Indian", "Mediterranean"
+  ]
+  public features: string[] = [
+    "-- Options --", "MeatLover", "Vegetarian", "LowCarb", "Vegan"
+  ]
+
+  public imagePath: string ="";
+  public imgURL: any = "";
+  public message: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  preview(files: any) {
+    if (files.length === 0)
+      return;
+ 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+    this.imgURL = reader.result; 
+    }
+  }
+
+  onUpload() {
+    // upload code goes here
   }
 
 }
