f506ec78d8b68db012d2767fb167c1af13c7eb11
tongding <dingx170@gmail.com>
2021-02-17 02:49:29 -0800

    Updated home page and user page

--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
+  public recipes: any[] = [
+    {name: "Supreme Steak", image: "recipe_1.jpg"},
+    {name: "Potato Beef Tender", image: "recipe_2.jpg"},
+    {name: "Avacado Gimbap", image: "recipe_5.jpg"},
+    {name: "Garden Salad", image: "recipe_4.jpg"},
+    {name: "Roast Salmon", image: "recipe_6.jpg"},
+    {name: "Berry Sandwich", image: "recipe_7.jpg"},
+    {name: "Avacado Gimbap", image: "recipe_5.jpg"},
+    {name: "Supreme Steak", image: "recipe_1.jpg"},
+    {name: "Potato Beef Tender", image: "recipe_2.jpg"}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
