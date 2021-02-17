827b1d2bad87a2e5aa558c509d2e9094e141a64e
Chenjie Fan <fanchenjie@seattleu.edu>
2021-02-17 01:52:28 -0800

    Added newrecipe page

--- src/app/components/myrecipe/newrecipe/newrecipe.component.ts
+++ src/app/components/myrecipe/newrecipe/newrecipe.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewrecipeComponent implements OnInit {
 
+  public step_list:any[] = [
+    {step: "weight 300g flour"},
+    {step: "prepare one cup oil"},
+    {step: "crack 2 eggs"},
+  ];
+
+  public ingredient_list:any[] = [
+    {name: "flour", unit: "gram", count:300},
+    {name: "oil", unit: "cup", count:1},
+    {name: "egg", unit: "gram", count:150},
+  ];
   constructor() { }
 
   ngOnInit(): void {
