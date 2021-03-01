46b270fcc80db02200d892b4e78371f33849b710
tongding <dingx170@gmail.com>
2021-03-01 15:50:46 -0800

    Added reactive form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -47,6 +48,7 @@ import { RecipelistComponent } from './components/mealplan/recipelist/recipelist
     AppRoutingModule,
     NgbModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     HttpClientModule
   ],
