c024b987caf80ffbff78f8908dc92001225289b9
Chenjie Fan <fanchenjie@seattleu.edu>
2021-03-01 17:22:29 -0800

    Added retieve all mealplans for user(from backend)

--- src/app/components/mealplan/oldplan/oldplan.component.ts
+++ src/app/components/mealplan/oldplan/oldplan.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IMealplan } from 'src/app/interfaces/imealplan';
+
+import { MealplanService } from '../../../services/mealplan.service';
 
 @Component({
   selector: 'app-oldplan',
@@ -7,22 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OldplanComponent implements OnInit {
 
-  public recipe_list:any[] = [
-    {time: "02/15/2021 05:00pm"},
-    {time: "01/21/2021 03:21pm"},
-    {time: "01/15/2021 10:05am"},
-    {time: "12/20/2021 07:10pm"},
-    {time: "12/11/2021 06:16pm"},
-    {time: "11/07/2021 03:00pm"},
-    {time: "10/05/2021 05:00pm"},
-    {time: "10/01/2021 06:16pm"},
-    {time: "09/15/2021 05:00pm"},
-    {time: "09/21/2021 03:21pm"}
-  ];
+  public mealplans: IMealplan[] = [];
+  constructor(
+    public mealplan:MealplanService
+    ) {
+      this.mealplan.getAllMealPlansForUser(123)
+      .subscribe((result)=>{
+        console.log(result);
+        this.mealplans = <IMealplan[]>result;
+        console.log(this.mealplans);
+      })
+     }
 
-  constructor() { }
+  // ngOnInit(): void {
+  //   this.mealplans = this.mealplan.getAllMealPlansForUserTest();
+  // }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
