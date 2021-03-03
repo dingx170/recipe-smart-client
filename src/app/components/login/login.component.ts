99e253bf7514cb7e6f22d1339a5e45dbea807216
Hao <roadmonster1989@gmail.com>
2021-03-02 16:58:36 -0800

    updated login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IAuth } from 'src/app/interfaces/auth';
+import { LoginRes } from 'src/app/interfaces/loginRes';
+import { LoginService } from '../../services/login.service'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  auth: IAuth = {
+    username: "test username",
+    password: "test password"
+  };
+
+  current_userid: number;
+  login_response: LoginRes;
+
+  constructor(private login_service: LoginService) { }
 
   ngOnInit(): void {
+    this.auth.username = "test username";
+    this.auth.password = "test password";
+  }
+
+  login():void{
+    console.log(this.auth.username);
+    console.log(this.auth.password);
+    this.login_service.login(this.auth).subscribe(
+      res => {
+
+        if(res.ret_code == 0){
+          this.current_userid = res.userid;
+          console.log(res.ret_msg);
+        }
+        this.login_response = res;
+      }
+
+    );
   }
 
 }
