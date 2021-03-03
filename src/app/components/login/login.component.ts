ccbe660bfd5414582b2a30f9554978907b8f34da
Hao <roadmonster1989@gmail.com>
2021-03-02 18:17:40 -0800

    login component shall take the response user id and put it into share data service for other component to access

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,12 +2,18 @@ import { Component, Input, OnInit } from '@angular/core';
 import { IAuth } from 'src/app/interfaces/auth';
 import { LoginRes } from 'src/app/interfaces/loginRes';
 import { LoginService } from '../../services/login.service'
+import { ShareDataService } from '../../services/share-data.service'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+/**
+ * This service send login request to backend and calls for storeage service
+ * storage service is exposed for other component to use
+ */
 export class LoginComponent implements OnInit {
 
   @Input()
@@ -16,24 +22,21 @@ export class LoginComponent implements OnInit {
     password: "test password"
   };
 
-  current_userid: number;
   login_response: LoginRes;
 
-  constructor(private login_service: LoginService) { }
+  constructor(private login_service: LoginService,
+              private shared_service: ShareDataService) { }
 
   ngOnInit(): void {
-    this.auth.username = "test username";
-    this.auth.password = "test password";
   }
 
   login():void{
-    console.log(this.auth.username);
-    console.log(this.auth.password);
+
     this.login_service.login(this.auth).subscribe(
       res => {
 
         if(res.ret_code == 0){
-          this.current_userid = res.userid;
+          this.shared_service.setData("userid", res.userid);
           console.log(res.ret_msg);
         }
         this.login_response = res;
