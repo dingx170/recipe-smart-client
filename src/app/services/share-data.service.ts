c8e991d7c5496afbc9bfb954d1c4a88b1ffee035
Hao <roadmonster1989@gmail.com>
2021-03-02 18:16:25 -0800

    share data service provide in memory cache function

--- src/app/services/share-data.service.ts
+++ src/app/services/share-data.service.ts
@@ -3,7 +3,25 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
+/**
+ * This is a util service to store the temporary data returned from the backend
+ */
 export class ShareDataService {
 
+  data:any={};
+
   constructor() { }
+
+  setData(key: any,value: any) {
+    this.data[key]=value;
+ }
+
+ clearData() {
+    this.data={};
+ }
+
+ getData(key: any) {
+   return this.data[key];
+ }
 }
