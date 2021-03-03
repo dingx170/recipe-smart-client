dbfdf345ea50405369b805c5374fe0f5b59c460c
Hao <roadmonster1989@gmail.com>
2021-03-02 16:56:21 -0800

    updated login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of} from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { IAuth } from '../interfaces/auth';
+import { LoginRes } from '../interfaces/loginRes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private base_api:string = "http://localhost:8080";
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Login service returns the user id as a temporarty token
+   * @param auth username password combination
+   *
+   */
+  login(auth: IAuth): Observable<LoginRes>{
+    let url = this.base_api + "/login";
+    return this.http.post<LoginRes>(url, auth, this.httpOptions).pipe(
+      catchError(this.handleError<LoginRes>('user login failed')
+    ));
+  }
+
+  /**
+   * Handle the error and return a human readable message to delegating component
+   * Return an empty IUser object to keep the app running.
+   * @param operation
+   * @param result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+
+
 }
