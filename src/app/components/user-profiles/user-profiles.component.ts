45d3701c7a588ecd1a776e7bf74353a88bb088ed
Hao <roadmonster1989@gmail.com>
2021-03-04 01:49:07 -0800

    updated user-profile component

--- src/app/components/user-profiles/user-profiles.component.ts
+++ src/app/components/user-profiles/user-profiles.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FoodAllergy } from 'src/app/enums/food-allergy.enum';
 import { IUser } from 'src/app/interfaces/IUser';
 import { UserService} from '../../services/user.service'
-import { UserTag} from '../../enums/user-tag.enum'
 import { ShareDataService} from '../../services/share-data.service'
 
 @Component({
@@ -13,18 +12,16 @@ import { ShareDataService} from '../../services/share-data.service'
 export class UserProfilesComponent implements OnInit {
 
   @Input()
-  user: IUser = {
-    user_name: "zhangsan",
-    password: "123",
-    email: "1234@gmail.com",
-    restrictions: [FoodAllergy.Corn, FoodAllergy.Egg],
-    userTags: [UserTag.Expert, UserTag.Chef]
+  user: IUser;
 
-  };
+  private userid: number;
 
-  userid: number;
+  logged_in: boolean = false;
+  updated_profile: boolean = false;
+  update_fail: boolean = false;
 
   allergy_list = Object.keys(FoodAllergy) as FoodAllergy[];
+
 
 
   constructor(
@@ -34,33 +31,54 @@ export class UserProfilesComponent implements OnInit {
 
   ngOnInit(): void {
     this.userid  = this.share_service.getData("userid");
-    // There might be issues when the data structure does not match the front end
+
+    //every time initializing this component default logged in as false;
+    this.logged_in = false;
+    this.updated_profile = false;
+    this.update_fail = false;
+    // Will only display profiles after logged in. Otherwise display default content html.
     if(this.userid){
       this.user_service.getUserById(this.userid).subscribe(res => {
-        this.user = res;
+        if(res.ret_code == 0){
+          this.user = res.user_obj;
+          this.logged_in = true;
+        }
       });
     }
 
   }
-
+  /**
+   * update user with the user object held within this component
+   */
   update():void{
     if(this.userid){
       // response from the backend needs to be structured as loginRes
       this.user_service.updateUser(this.user, this.userid)
         .subscribe(res => {
-            console.log(res.ret_code)
-            console.log(res.ret_msg);
+            if(res.ret_code == 0){
+              this.updated_profile = true;
+            }
+            else if(res.ret_code == -1){
+              this.update_fail = true;
+            }
+
         });
     }
 
   }
 
-  createUser(user: IUser){
-    this.user_service.addUser(user).subscribe();
+  /**
+   * create user with the IUser object and update local cache in sharedata service
+   */
+  createUser(): void{
+    this.user_service.addUser(this.user).subscribe(res => {
+      if(res.ret_code == 1){
+        this.share_service.setData("userid", res.userid);
+      }
+      else if(res.ret_code == 0){
+        console.log(res.ret_msg);
+      }
+    });
   }
-
-
-
-
 
 }
